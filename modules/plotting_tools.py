f81707aec5b8953d0c23865746e7674226d4ff24
Ludovic Righetti <ludovic.righetti@gmail.com>
2011-11-15 23:05:15 -0800

    changed some code - read/plot figures fast

--- modules/plotting_tools.py
+++ modules/plotting_tools.py
@@ -2,33 +2,38 @@ from pylab import *
 from scipy import *
 import numpy
 
+
 def plot_result(filename):
     
+    #data = genfromtxt(filename, dtype = float)
+#    t, phi, omega = loadtxt(filename, unpack = True)
     with open(filename) as f:
-        t = numpy.array([double(line.split()[0]) for line in f])
-    with open(filename) as f:
-        phi = numpy.array([double(line.split()[1]) for line in f])
-    with open(filename) as f:
+        t = numpy.array([float(line.split()[0]) for line in f])
+#    with open(filename) as f:
+        f.seek(0)
+        phi = numpy.array([float(line.split()[1]) for line in f])
+#    with open(filename) as f:
+        f.seek(0)
         omega = numpy.array([double(line.split()[2]) for line in f])
     
-    if omega[-1] > 100 or omega[-1] < 0:
-        print filename
-        figure(1)
-        subplot(211)
-        plot(t, phi - 30 * t)
-        subplot(212)
-        plot(t, omega)
+#    t = data[:,0]
+#    phi = data[:,1]
+#    omega = data[:,2]
+#    
+    figure(1)
+    subplot(211)
+    plot(t, phi - 30 * t)
+    subplot(212)
+    plot(t, omega)
     
-        figure(2)
-        plot(mod(phi, 2*pi), omega)
-    
+#    figure(2)
+#    plot(mod(phi, 2*pi), omega)
     
 def plot_all():
-    for i in range(0,627):
+    
+    for i in range(0,627, 1):
         print i
         plot_result('../test_result' + str(i) + '.dat')
-              
-               
     show()
 
         
--- src/afo_systematic_test_main.cpp
+++ src/afo_systematic_test_main.cpp
@@ -22,37 +22,41 @@ int main(int argc, char** argv)
 
 
   double d_phi = 0.01;
-
+  double T = 100.0;
+  double dt = 0.1;
+  double omegaF = 30.0;
+  double K = 1000.0;
+  double omega0 = 10.0;
 
 #pragma omp parallel for
-  for(int j=0; j<int(6.28/d_phi);j++)
-  {
-    x_init[0] = double(j)*d_phi;
-    x_init[1] = 10.0;
+      for(int j=0; j<int(6.28/d_phi);j++)
+      {
+        x_init[0] = double(j)*d_phi;
+        x_init[1] = omega0;
 
-    std::vector<std::vector<double> > x_result;
-    std::vector<double> time;
+        std::vector<std::vector<double> > x_result;
+        std::vector<double> time;
 
-    afos::AdaptivePhaseOscillator afo(30.0, 1000.0);
-    afos::StateRecorder<std::vector<double> > recorder(x_result, time);
+        afos::AdaptivePhaseOscillator afo(omegaF, K);
+        afos::StateRecorder<std::vector<double> > recorder(x_result, time);
 
 
-    std::cout << "iteration " << j << std::endl;
-    size_t steps = boost::numeric::odeint::integrate(afo, x_init, 0.0, 10.0, 0.1, recorder);
+        std::cout << "iteration " << j << std::endl;
+        size_t steps = boost::numeric::odeint::integrate(afo, x_init, 0.0, T, dt, recorder);
 
-    std::stringstream filename;
-    filename << "test_result" << j << ".dat";
+        std::stringstream filename;
+        filename << "test_result" << j << ".dat";
 
-    std::fstream my_file(filename.str().c_str(), std::fstream::out);
+        std::fstream my_file(filename.str().c_str(), std::fstream::out);
 
-    for(size_t i=0; i<=steps; i++)
-    {
-      my_file << time[i] << '\t'
-          << x_result[i][0] << '\t' << x_result[i][1] << '\n';
-    }
-    my_file.close();
+        for(size_t i=0; i<=steps; i++)
+        {
+          my_file << time[i] << '\t'
+              << x_result[i][0] << '\t' << x_result[i][1] << '\n';
+        }
+        my_file.close();
 
-  }
+      }
 
   return 0;
 }
